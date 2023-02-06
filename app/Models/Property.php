ace088a3cbe82ce2428b865573b06c5ddc894e7c
UniversoCaotico1997 <loria.roberto97@gmail.com>
2023-02-06 16:51:54 +0100

    close #6gestione-users

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -5,15 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Property extends Model
 {
-    protected $fillable = ['title', 'slug', 'price', 'description', 'rooms_num', 'beds_num', 'baths_num', 'square_meters', 'street', 'city',  'state', 'image', 'visibility', 'longitude', 'latitude'];
+    protected $fillable = ['title', 'slug', 'price', 'description', 'rooms_num', 'beds_num', 'baths_num', 'square_meters', 'street', 'city',  'state', 'image', 'visibility', 'longitude', 'latitude', 'user_id'];
     use HasFactory;
 
     public static function generateSlug($title)
     {
         $property_slug = Str::slug($title);
         return $property_slug;
+    }
+
+
+
+    /**
+     * Get the user_id that owns the Property
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
---
+++ database/migrations/2023_02_06_150929_add_user_id_to_properties_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable()->after('id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->dropForeign('properties_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
+    }
+};
