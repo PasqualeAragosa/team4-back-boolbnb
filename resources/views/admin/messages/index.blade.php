7e79689fc47d38189ca75c04dd5bdee988805868
Pasquale Aragosa <aragosap@gmail.com>
2023-02-10 15:47:36 +0100

    fix messages

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -14,57 +14,30 @@
                 <td>Name</td>
                 <td>Email</td>
                 <td>Phone</td>
-                <td>Property ID</td>
+                <td>Property</td>
                 <td colspan="2">Message</td>
-                <td>Actions</td>
             </tr>
         </thead>
-        <tbody >
+        <tbody>
             @forelse ($messages as $message)
-                <tr>
-                    <td> {{ $message->guest_full_name }} </td>
-                    <td> {{ $message->guest_email }} </td>
-                    <td> {{ $message->guest_phone_number }} </td>
-                    <td> 
-                            {{ $message->property_id }}
-
-                    </td>
-                    <td colspan="2">{{ $message->content }}</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btn-md" data-bs-toggle="modal" data-bs-target="#deleteMessage-{{$message->id}}">
-                        <i class="fa-solid fa-trash-can"></i>
-                        </button>
-                    <!-- Modal Body -->
-                            <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                            <div class="modal fade" id="deleteMessage-{{$message->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId-{{$message->id}}" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="modalTitleId-{{$message->id}}">Delete message</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            Are you sure, you want to delete the message? The process is irreversible!
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                            <form action="{{route('admin.messages.destroy', $message->id)}}" method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="w-100 btn btn-danger"> CONFIRM
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                </td>                   
-                </tr>
-                @empty
-                <tr class="table-primary">
-                    <td scope="row">Sorry no messages to show</td>
-                </tr>
-                @endforelse
+            <tr>
+                <td> {{ $message->guest_full_name }} </td>
+                <td> {{ $message->guest_email }} </td>
+                <td> {{ $message->guest_phone_number }} </td>
+                <td>
+                    @foreach($properties as $property)
+                    @if($message->property_id == $property->id)
+                    {{$property->title}}
+                    @endif
+                    @endforeach
+                </td>
+                <td colspan="2">{{ $message->content }}</td>
+            </tr>
+            @empty
+            <tr class="table-primary">
+                <td scope="row">Sorry no messages to show</td>
+            </tr>
+            @endforelse
         </tbody>
     </table>
 </div>
