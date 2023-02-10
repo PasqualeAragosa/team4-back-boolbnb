4e4e3bb58481bfd4dd16e66b197e513164ab4be3
snerva <selenenerva@live.it>
2023-02-10 15:52:48 +0100

    addlayouts scss style

--- resources/views/admin/properties/show.blade.php
+++ resources/views/admin/properties/show.blade.php
@@ -1,47 +1,68 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="content-header pt-5 px-5">
-    <h1 class="text-orange">{{$property->title}}</h1>
-    <!-- if there's an image, show it; otherwise, show a placeholder -->
-    @if($property->image)
-    <img class="img-fluid w-50" src="{{asset('storage/' . $property->image)}}" alt="">
-    @else
-    <div class="placeholder p-5 bg-secondary">Placeholder</div>
-    @endif
-   
+<div class="content-header p-5">
+    <div class="row row-cols-1 row-cols-md-2">
+        <div class="col d-flex justify-content-center align-items-center">
+            <div class="property_title text-center px-3">
+                <h1 class="text-orange">{{$property->title}}</h1>
+                <p>Address: {{$property->address}}</p>
+                <hr>
+                <h2 class="text-orange">{{$property->price}} &euro;</h2>
+                @if($property->visibility)
+                <h3 class="text-orange">Available</h3>
+                @else
+                <h3>Not available</h3>
+                @endif
+            </div>
+        </div>
+        <div class="col">
+           <div class="img-gallery overflow-hidden">
+             <!-- if there's an image, show it; otherwise, show a placeholder -->
+             @if($property->image)
+             <img class="img-fluid photo-zoom" src="{{asset('storage/' . $property->image)}}" alt="">
+             @else
+             <div class="placeholder p-5 bg-secondary">Placeholder</div>
+             @endif
+           </div>
+        </div>
+    </div>
 </div>
 <div class="content-body pt-3 px-5">
-<div class="info d-flex gap-5">
-    <p class="text-orange">{{$property->price}} &euro;</p>
-<p class="text-orange"><i class="fa-solid fa-door-closed"></i> {{$property->rooms_num}}</p>
-<p class="text-orange"><i class="fa-solid fa-bed"></i> {{$property->beds_num}}</p>
-<p class="text-orange">{{$property->square_meters}} m</p>
-</div>
-<p>Address: {{$property->address}}</p>
+    <div class="row row-cols-1 row-cols-md-2">
+        <div class="col text-center">
+            <div class="info d-flex justify-content-around mb-3">
+                <h4 class="text-orange"><i class="fa-solid fa-door-closed"></i> {{$property->rooms_num}}</h4>
+                <h4 class="text-orange"><i class="fa-solid fa-bed"></i> {{$property->beds_num}}</h4>
+                <h4 class="text-orange">{{$property->square_meters}} &#13217;</h4>
+            </div>
+            <div class="description mb-3">
+                {{$property->description}}
+            </div>
+            <!-- type -->
+            <div class="amenities">
+                <h5 class="text-orange">Amenities:</h5>
+                @if(count($property->amenities) > 0 )
+            
+            
+                @foreach ($property->amenities as $amenity)
+                <span>{{$amenity->name}} </span>
+                @endforeach
+            
+                @else
+                <span>Not Amenities associated to the property</span>
+                @endif
+            
+            </div>
+        </div>
+        <div class="col">
+           <div class="maps text-center">
+            ...MAPS HERE!
+           </div>
+        </div>
+    </div>
 
-@if($property->visibility)
-<h5 class="text-orange">Available</h5>
-@else
-<h5 class="text-orange">Not available</h5>
-@endif
-<div class="description mb-3">
-    {{$property->description}}
-</div>
-<!-- type -->
-<div class="amenities">
-    <span>Amenities:</span>
-    @if(count($property->amenities) > 0 )
 
 
-    @foreach ($property->amenities as $amenity)
-    <span>{{$amenity->name}}</span>
-    @endforeach
-
-    @else
-    <span>Not Amenities associated to the property</span>
-    @endif
-
-</div>
 </div>
 @endsection
--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -4,11 +4,11 @@
     <div class="content-header py-5">
         <h1 class="text-orange text-center">Choose Your Sponsorship</h1>
     </div>
-    <div class="row row-cols-1 row-cols-md-2 g-4 py-5">
-        <div class="col">
+    <div class="row row-cols-1 row-cols-lg-2 g-4 py-5">
+        <div class="col col-lg-4">
             <div class="property mb-5 px-4">
                 <label for="property_id" class="form-label text-orange"><h5>Please, select your Property</h5></label>
-                <select class="form-select form-select-md w-75" name="property_id" id="property_id">
+                <select class="form-select form-select-md" name="property_id" id="property_id">
                     <option disabled value=''>Select one</option>
                     @forelse ($properties as $property)
                     <option value="{{ $property->id }}" {{ $property->id == old('property_id') ? 'selected' : '' }}>
@@ -20,16 +20,18 @@
                 </select>
             </div>
             <!-- /.Property -->
+        </div>
+        <!-- /.col -->
+        <div class="col col-lg-8">
             <div class="sponsorships pb-4 px-4">
                 <h5 class="text-orange mb-4">Check the Sponsorship</h5>
-
                 <div class="row row-cols-1 row-cols-md-3">
                     <div class="col">
                         <div class="range text-center border border-secondary rounded-3 p-3">
                             <h6 class="text-secondary">{{$sponsorships[0]->duration}} Hours</h6>
                         <p class="text-secondary">{{$sponsorships[0]->price}} &euro;</p>
                         <input type="radio" class="btn-check" name="options-outlined" id="secondary-outlined" autocomplete="off" checked>
-                        <label class="btn btn-outline-secondary w-75" for="secondary-outlined">{{$sponsorships[0]->name}}</label>
+                        <label class="btn btn-outline-secondary" for="secondary-outlined">{{$sponsorships[0]->name}}</label>
                         </div>
                     </div>
 
@@ -38,7 +40,7 @@
                             <h6 class="text-warning">{{$sponsorships[1]->duration}} Hours</h6>
                         <p class="text-warning">{{$sponsorships[1]->price}} &euro;</p>
                         <input type="radio" class="btn-check" name="options-outlined" id="warning-outlined" autocomplete="off">
-                        <label class="btn btn-outline-warning w-75" for="warning-outlined">{{$sponsorships[1]->name}}</label>
+                        <label class="btn btn-outline-warning" for="warning-outlined">{{$sponsorships[1]->name}}</label>
                         </div>
                     </div>
                     
@@ -47,7 +49,7 @@
                             <h6 class="text-primary">{{$sponsorships[2]->duration}} Hours</h6>
                         <p class="text-primary">{{$sponsorships[2]->price}} &euro;</p>
                         <input type="radio" class="btn-check" name="options-outlined" id="primary-outlined" autocomplete="off">
-                        <label class="btn btn-outline-primary w-75" for="primary-outlined">{{$sponsorships[2]->name}}</label>
+                        <label class="btn btn-outline-primary" for="primary-outlined">{{$sponsorships[2]->name}}</label>
                         </div>
                         
                     </div>
@@ -56,10 +58,9 @@
             </div>
             <!-- /.Sponsorship -->
         </div>
-        <!-- /.col -->
-        <div class="col">
-        </div>
-        <!-- /.col -->
     </div>
     <!-- /.row -->
+    <div class="payments text-center py-5">
+        >>>PAYMENTS HERE!
+    </div>
 @endsection
