fef796ee25ca6a3fc4bf0d66fb37c8c1707c798d
snerva <selenenerva@live.it>
2023-02-15 16:10:47 +0100

    message controller

--- config/cors.php
+++ config/cors.php
@@ -25,7 +25,7 @@ return [
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => false,
 
     'max_age' => 0,
 
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\API\MessageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Property;
-use App\Models\Message;
-use App\Http\Controllers\API\PropertyController;
+use App\Http\Controllers\Api\PropertyController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -25,5 +24,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/properties', [PropertyController::class, 'index']);
 Route::get('/properties/{property:slug}', [PropertyController::class, 'show']);
 Route::get('/properties/search/lng={lng}/lat={lat}/radius={radius}', [PropertyController::class, 'searchProperties'])->name('apartments.search');
-Route::post('/messages', [MessageController::class, 'store']);
-
