60bdb0c7892a2e6a3abdb9165de79f443d48f152
snerva <selenenerva@live.it>
2023-02-17 12:10:52 +0100

    background home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-white rounded-3 vh-100 ">
-    <div class="container py-5 text-center">
-        <div class="logo mb-5">
-            <img src="/images/logo.png" class="w-25" alt="">
-        </div>
-        <h1 class="display-5 fw-bold mb-5">
-            Welcome
-        </h1>
-
-        <button class="btn btn-outline-secondary btn-lg" type="button">Example</button>
-    </div>
+<div class="jumbotron welcome p-5 mb-4 bg-white rounded-3 vh-100 ">
+    
 </div>
 
 @endsection
